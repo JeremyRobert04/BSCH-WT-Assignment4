ddb730a460771a467e11179cae2001636304c9ca
Jérémy <72013008+JeremyRobert04@users.noreply.github.com>
2024-05-14 11:47:32 +0200

    add basic layout & routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend';
+  opened = false;
+  faCircleChevronRight = faCircleChevronRight;;
+
+  public hamburgerAnimation(event: MouseEvent): void {
+    const button = event.currentTarget as HTMLButtonElement;
+    const currentState = button.getAttribute('data-state');
+
+    if (!currentState || currentState === 'closed') {
+      button.setAttribute('data-state', 'opened');
+      button.setAttribute('aria-expanded', 'true');
+    } else {
+      button.setAttribute('data-state', 'closed');
+      button.setAttribute('aria-expanded', 'false');
+    }
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
+];
