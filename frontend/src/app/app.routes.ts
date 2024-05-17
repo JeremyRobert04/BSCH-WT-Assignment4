731090db8327fd6e23cc30d507e8203ed2fa971a
Jérémy <72013008+JeremyRobert04@users.noreply.github.com>
2024-05-17 15:10:44 +0200

    create component & home page navigation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'categories', component: CategoriesComponent },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
 ];
