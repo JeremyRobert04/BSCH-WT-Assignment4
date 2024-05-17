0bf063db58cd88d72c413936bd60e6a900ca4b4c
Ashwin <ashwin.sagodira@epitech.eu>
2024-05-17 18:27:36 +0100

    [ADD] page login + register

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  imports: [HttpClientModule]
 })
 export class LoginComponent {
+  title = 'Login Page';
 
+  constructor(private http: HttpClient) {}
+
+  userLogin(email: string, password: string) {
+    const loginData = { email, password };
+
+    this.http.post('http://localhost:8080/login', loginData)
+    .subscribe(response => {
+      console.log('Login successful:', response);
+      window.location.href = '/';
+      
+    }, error => {
+      console.error('Login failed:', error);
+      console.error('Complete error object:', error);
+    });
+  }
 }
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  title = 'Register Page';
 
+  constructor(private http: HttpClient) { }
+
+  userRegister(firstName: string, lastName: string, email: string, password: string, confirmPassword: string) {
+    const professional = false;
+    const verified = true;
+    
+    const registerData = { firstName, lastName, email, password, professional, verified};
+
+    this.http.post('http://localhost:8080/register', registerData)
+    .subscribe(response => {
+      console.log('Registration successful:', response);
+      window.location.href = '/login';
+    }, error => {
+      console.error('Registration failed:', error);
+      console.error('Complete error object:', error);
+    });
+  }
 }
